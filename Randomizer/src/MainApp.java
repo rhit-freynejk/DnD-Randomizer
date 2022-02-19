7e064351638f6f5975e68779096896a4acbc16cf
rhit-freynejk <freynejk@rose-hulman.edu>
2022-02-19 15:37:05 -0500

    Starting

---
+++ Randomizer/src/MainApp.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class MainApp {
+	
+	private String bigClass;
+	private String subClass;
+	private int level;
+	private ArrayList<String> feats;
+	private ArrayList<String> styles; // Invocations, Fighting Styles, Infusions, all that type of thing
+	
+	public MainApp() {
+		bigClass = "";
+		subClass = "";
+		level = 0;
+		feats = new ArrayList<String>(0);
+		styles = new ArrayList<String>(0);
+	}
+	
+	public void popClass(int level) {
+		bigClass = 
+		subClass
+	}
+
+}
